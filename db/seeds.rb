1af0f68b186affc71fb0ad1c55c7b2464a05562b
hashryo <ryo_hashimoto1108@icloud.com>
2019-09-03 15:33:21 +0900

    シードファイル編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Prefecture.create([{ prefecture: "Hokkaido" }, { prefecture: "Aomori" }, { prefecture: "Iwate" },
+                    { prefecture: "Miyagi" }, { prefecture: "Akita" }, { prefecture: "Yamagata" },
+                    { prefecture: "Fukushima" },{ prefecture: "Ibaraki" }, { prefecture: "Tochigi" }, 
+                    { prefecture: "Gunma" },{ prefecture: "Saitama" }, { prefecture: "Chiba" }, { prefecture: "Tokyo" },
+                    { prefecture: "Kanagawa" },{ prefecture: "Niigata" }, { prefecture: "Toyama" }, { prefecture: "Ishikawa" },
+                    { prefecture: "Fukui" }, { prefecture: "Yamanashi" }, { prefecture: "Nagano" },
+                    { prefecture: "Gifu" },{ prefecture: "Shizuoka" }, { prefecture: "Aichi" }, 
+                    { prefecture: "Mie" },{ prefecture: "Shiga" }, { prefecture: "Kyoto" }, { prefecture: "Osaka" },
+                    { prefecture: "Hyogo" },{ prefecture: "Nara" }, { prefecture: "Wakayama" }, { prefecture: "Tottori" },
+                    { prefecture: "Shimane" }, { prefecture: "Okayama" },
+                    { prefecture: "Hiroshima" },{ prefecture: "Yamaguchi" }, { prefecture: "Tokushima" }, 
+                    { prefecture: "Kagawa" },{ prefecture: "Ehime" }, { prefecture: "Kochi" }, { prefecture: "Fukuoka" },
+                    { prefecture: "Saga" },{ prefecture: "Nagasaki" }, { prefecture: "Kumamoto" }, 
+                    { prefecture: "Oita" },{ prefecture: "Miyazaki" }, { prefecture: "Kagoshima" }, { prefecture: "Okinawa" },])
